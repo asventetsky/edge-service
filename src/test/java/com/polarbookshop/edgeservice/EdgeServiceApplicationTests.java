4a6e01eb75e15b843df214e3405b687dc0701358
asventetsky <artyom.sven@gmail.com>
2024-10-19 21:55:47 +0200

    Artifact based on commit sha + acceptance stage + test fix

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -7,6 +7,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
@@ -17,8 +19,11 @@ class EdgeServiceApplicationTests {
 	private static final int REDIS_PORT = 6379;
 
 	@Container
-	static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.0"))
+	static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.2"))
 			.withExposedPorts(REDIS_PORT);
+
+	@MockBean
+	ReactiveClientRegistrationRepository clientRegistrationRepository;
 
 	@DynamicPropertySource
 	static void redisProperties(DynamicPropertyRegistry registry) {
